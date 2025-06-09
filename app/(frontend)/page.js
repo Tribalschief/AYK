d442dd1fd3d941f28fe822c522a3ea15d0f7d85a
TribalChief <acknowledgeyourking@gmail.com>
2025-06-10 03:40:39 +0500

    add 3d

--- app/(frontend)/page.js
+++ app/(frontend)/page.js
@@ -1,34 +1,20 @@
-'use client';
 
-import VideoSection from '@/components/VS/VS'
+import VideoSection from '@/components/VS/VS';
 import UltimateHome from '../../components/Intro';
 import UltimateServices from '../../components/Services';
 import UltimatePortfolioContact from '../../components/Portfolio';
-import { useState, useEffect } from 'react';
-
-
-
-
 
 export default function Home() {
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  if (!isClient) {
-    return null; // Render nothing on the server
-  }
+  
 
   return (
-    <div className="App">
+    <div className="App" ref={pageRef}>
       <VideoSection />
       <div>
-      <UltimateHome />
-      <UltimateServices />
-      <UltimatePortfolioContact />
+        <UltimateHome />
+        <UltimateServices />
+        <UltimatePortfolioContact />
       </div>
-  </div>
+    </div>
   );
 }
